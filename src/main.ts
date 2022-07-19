8f730518cf1caae8f9128a6d8ba444e76b16b48c
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-07-18 22:34:16 -0300

    chore: setup relative paths

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-console.log('Hello World');
+import { foo } from '@engine';
+import { bar } from '@game';
+
+console.log('Hello World', foo, bar);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [eslint()]
+    plugins: [tsconfigPaths(), eslint()]
 });
