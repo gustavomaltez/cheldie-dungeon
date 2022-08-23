9154ca28c722236ce24009fcaaa89e538de4f917
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-08-23 12:35:12 -0300

    test: added getRandomHexColor basic tests

---
+++ src/tests/utils/getRandomHexColor.spec.ts
@@ -0,0 +1,16 @@
+import { getRandomHexColor } from '@utils/getRandomHexColor';
+
+describe('Random Hex Color Utility Function', () => {
+
+  it('should return a string.', () => {
+    expect(typeof getRandomHexColor('testing')).toBe('string');
+  });
+
+  it('Should return an hexadecimal string.', () => {
+    expect(getRandomHexColor('testing')).toMatch(/^[0-9A-F]{6}$/i);
+  });
+
+  it('Should return an string whit 6 characters.', () => {
+    expect(getRandomHexColor('testing').length).toBe(6);
+  });
+});
