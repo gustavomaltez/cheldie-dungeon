e8c9e415fd98d2b0497f431e0fbf055a7f1274b4
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-08-24 08:46:46 -0300

    refactor: changed logic to generate random ids

--- src/utils/getRandomId.ts
+++ src/utils/getRandomId.ts
@@ -1,8 +1,7 @@
-import { nanoid } from 'nanoid';
 
 /**
  * Generates a unique string of characters.
  */
 export function getRandomId(): string {
-  return nanoid();
+  return Date.now().toString(36) + Math.random().toString(36).substring(2);
 }
