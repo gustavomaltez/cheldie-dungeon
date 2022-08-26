cf0fb10ad1b56fdc78290574639600d65b756eb7
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-08-26 10:10:35 -0300

    fix: fixed getRandomId logic to return ids with always the same length

--- src/utils/getRandomId.ts
+++ src/utils/getRandomId.ts
@@ -3,5 +3,7 @@
  * Generates a unique string of characters.
  */
 export function getRandomId(): string {
-  return Date.now().toString(36) + Math.random().toString(36).substring(2);
+  const start = (Date.now() * Math.random() * 1000).toString(36);
+  const end = (Math.random() * 10000).toString(36);
+  return (start + end).slice(0, 20);
 }
